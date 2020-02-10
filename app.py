29b83ef5d7778c2e5b96d0308f40e0e1fef3cb54
BryanSouza91 <40864284+BryanSouza91@users.noreply.github.com>
2020-02-09 16:28:31 -0800

    Worked on the routes and tables

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify, render_template
 from flask_pymongo import PyMongo
 from dtime import is_leap_year, ymd
+import pandas as pd
 
 
 # Setup Flask 
@@ -11,26 +12,23 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Project_2_db"
 mongo = PyMongo(app)
 
-# This is where we put functions to filter and sort
-fires_2004 = mongo.db.fires.find({"FIRE_YEAR": 2004})
-dates_2004 = []
-for v in fires_2004:
-    yr = v['FIRE_YEAR']
-    dy = v['DISCOVERY_DOY']
-    dates_2004.append(ymd(yr,dy))
-
 
 # Define Flask Routes
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
-@app.route("/table")
-def table_page():
-    return render_template("table.html",
-        dates_2004=dates_2004)
-
-
+@app.route("/table/<state0>/<state1>")
+def table_page(state0,state1):
+    df0 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state0 }))
+    df1 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state1 }))
+    return render_template("table.html", data={
+                                            "state": (state0,state1),
+                                            "causes": {
+                                                "state0": df0.STAT_CAUSE_DESCR.value_counts(),
+                                                "state1": df1.STAT_CAUSE_DESCR.value_counts()
+                                                }
+                                            })
 
 
 
