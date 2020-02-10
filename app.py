2b8ecb9effa14d3df8b34039417a0fce2f7b8010
BryanSouza91 <40864284+BryanSouza91@users.noreply.github.com>
2020-02-10 14:20:46 -0800

    worked on form and made table smaller

--- app.py
+++ app.py
@@ -27,14 +27,18 @@ def home_page():
         state_list = [state for state in states.STATE.unique()]
         state0 = request.form.get('state0')
         state1 = request.form.get('state1')
-        df0 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state0 },{"STAT_CAUSE_DESCR": 1}))
-        df1 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state1 },{"STAT_CAUSE_DESCR": 1}))
+        df0 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state0 },{"STAT_CAUSE_DESCR": 1, "STATE": 1}))
+        df1 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state1 },{"STAT_CAUSE_DESCR": 1, "STATE": 1}))
         return render_template("table.html", data={
                                                 "states": state_list,
                                                 "state": (state0,state1),
                                                 "causes": {
                                                     "state0": df0.STAT_CAUSE_DESCR.value_counts(),
                                                     "state1": df1.STAT_CAUSE_DESCR.value_counts()
+                                                    },
+                                                "years": {
+                                                    "state0": df0.STATE.value_counts(),
+                                                    "state1": df1.STATE.value_counts()
                                                     }
                                                 })
 
