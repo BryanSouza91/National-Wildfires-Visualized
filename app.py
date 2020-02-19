ba4d4a7bef68ec62e4ca88763d1be697a75d72e0
BryanSouza91 <40864284+BryanSouza91@users.noreply.github.com>
2020-02-19 15:06:14 -0800

    created dashboard

--- app.py
+++ app.py
@@ -1,61 +1,84 @@
 import numpy as np
+import pandas as pd
 import os
 from flask import Flask, jsonify, render_template, request
 from flask_pymongo import PyMongo
-from dtime import is_leap_year, ymd
-import pandas as pd
+import json
 
-
-# Setup Flask 
-
+# Setup Flask
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Project_2_db"
 mongo = PyMongo(app)
 
-
 # Define Flask Routes
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET"])
 def home_page():
     if request.method == "GET":
-        states = pd.DataFrame.from_records(mongo.db.fires.find({},{"STATE": 1}).limit(10000))
+        states_df = pd.DataFrame.from_records(
+            mongo.db.fires.find({"FIRE_SIZE": {"$gte": 1000 }}, {"STATE": 1}).limit(10000))
         state_list = []
-        state_list = [state for state in states.STATE.unique()]
+        state_list = [state for state in states_df.STATE.sort_values(ascending=True).unique()]
         return render_template("index.html", data={"states": state_list})
-    if request.method == "POST":
-        states = pd.DataFrame.from_records(mongo.db.fires.find({},{"STATE": 1}).limit(10000))
-        state_list = []
-        state_list = [state for state in states.STATE.unique()]
-        state0 = request.form.get('state0')
-        state1 = request.form.get('state1')
-        df0 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state0 },{"STAT_CAUSE_DESCR": 1}))
-        df1 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state1 },{"STAT_CAUSE_DESCR": 1}))
-        return render_template("table.html", data={
-                                                "states": state_list,
-                                                "state": (state0,state1),
-                                                "causes": {
-                                                    "state0": df0.STAT_CAUSE_DESCR.value_counts(),
-                                                    "state1": df1.STAT_CAUSE_DESCR.value_counts()
-                                                    },
-                                                "total": {
-                                                    "state0": df0.STAT_CAUSE_DESCR.count(),
-                                                    "state1": df1.STAT_CAUSE_DESCR.count()
-                                                    }
-                                                })
 
-# @app.route("/",methods=["POST"])
-# def table():
-#     state0 = request.form.get('state0')
-#     state1 = request.form.get('state1')
-#     df0 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state0 },{"STAT_CAUSE_DESCR": 1}))
-#     df1 = pd.DataFrame.from_records(mongo.db.fires.find({"STATE": state1 },{"STAT_CAUSE_DESCR": 1}))
-#     return render_template("table.html", data={
-#                                             "state": (state0,state1),
-#                                             "causes": {
-#                                                 "state0": df0.STAT_CAUSE_DESCR.value_counts(),
-#                                                 "state1": df1.STAT_CAUSE_DESCR.value_counts()
-#                                                 }
-#                                             })
+# get data function
+# create list of lists for highcharts
+@app.route("/api/<state0>", methods=["GET"])
+def get_data(state0):
 
+    # pulls in data based on api state variable
+    df0 = pd.DataFrame.from_records(mongo.db.fires.find(
+        {"STATE": state0,"FIRE_SIZE": {"$gte": 1000 }}, {"STAT_CAUSE_DESCR": 1, 'FIRE_SIZE': 1, 'FIRE_YEAR': 1, 'DAYS_TO_CONT': 1}))   # {"$gte": 10 }
+
+    # build pivot table for fire size
+    cause_size_pivot = pd.pivot_table(df0, values='FIRE_SIZE', index=['FIRE_YEAR'],
+                    columns=['STAT_CAUSE_DESCR'], aggfunc=np.sum, fill_value=0)
+
+    # structure data from pivot for streamgraph
+    years_categories = [str(x) for x in cause_size_pivot.index]
+    streamgraph_data = []
+    for each in cause_size_pivot.iteritems():
+        each_cause = {
+            "name": each[0],
+            "data": [round(v,2) for k,v in cause_size_pivot[each[0]].items()]
+        }
+        streamgraph_data.append(each_cause)
+
+    # structure data from pivot for variable pie
+    vpie_size_df = pd.DataFrame(df0.groupby(['STAT_CAUSE_DESCR']).FIRE_SIZE.sum())
+    vpie_count_df = pd.DataFrame(df0.STAT_CAUSE_DESCR.value_counts())
+    vpie__df = vpie_size_df.merge(vpie_count_df,left_index=True,right_index=True)
+    vpie_data = []
+    for each in vpie__df.iterrows():
+        each_slice = {
+            "name": each[0],
+            "y": round(each[1].FIRE_SIZE,2),
+            "z": round(each[1].STAT_CAUSE_DESCR)
+        }
+        vpie_data.append(each_slice)
+
+    # line chart 
+    cont_pivot = pd.pivot_table(df0, values='DAYS_TO_CONT', index=['FIRE_YEAR'],
+                    columns=['STAT_CAUSE_DESCR'], aggfunc=np.sum, fill_value=0)
+    cont_list = []
+    for index,data in cont_pivot.iterrows():
+        each_slice = {
+            "name": index,
+            "data": data
+        }
+        cont_list.append(each_slice)
+
+    # put into data variable for json
+    data = {
+        "column": {
+            "counts": [[k, v] for k, v in df0.STAT_CAUSE_DESCR.value_counts().items()],
+            },#"days_to_cont": cont_list },
+        "v_pie": vpie_data,
+        "streamGraph": {
+            "data": streamgraph_data,
+            "years_categories": years_categories },
+        
+    }
+    return data
 
 
 if __name__ == '__main__':
---
+++ static/js/test.js
@@ -0,0 +1,139 @@
+
+let form = document.getElementById('form');
+
+form.addEventListener('change', function () {
+  let st0_sel = document.getElementById("state0_sel");
+  let st0 = st0_sel.options[st0_sel.selectedIndex].value;
+
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", `/api/${st0}`, false); // false for synchronous request  
+  xmlHttp.send(null);
+  // console.log(JSON.parse(xmlHttp.responseText));
+  let D = JSON.parse(xmlHttp.responseText);
+  // console.log(D);
+
+  function buildPieChart(data, st0) {
+    Highcharts.chart('pieChart', {
+      chart: {
+        type: 'variablepie'
+      },
+      title: {
+        text: `Number of Fires and Area Burned by Cause in ${st0}`
+      },
+      credits: {
+        enabled: false
+      },
+      xAxis: {
+        categories: data.column.counts.map(x => x[0])
+      },
+      series: [{
+        name: st0,
+        minPointSize: 20,
+        innerSize: '25%',
+        zMin: 0,
+        data: data.v_pie
+      }]
+    });
+  };
+
+  function buildBarChart(data, st0) {
+    Highcharts.chart('barChart', {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: `Number of Fires by Cause in ${st0}`
+      },
+      credits: {
+        enabled: false
+      },
+      xAxis: {
+        categories: data.column.counts.map(x => x[0])
+      },
+      yAxis: [{
+        min: 0,
+        title: {
+          text: 'Counts of Fires'
+        }
+      }],
+      series: [{
+        name: 'Counts of Fires',
+        data: data.column.counts
+      }]
+    });
+  };
+
+  function buildStreamGraph(data, st0) {
+    var colors = Highcharts.getOptions().colors;
+    Highcharts.chart('streamGraph', {
+
+      chart: {
+        type: 'streamgraph',
+        marginBottom: 30,
+        zoomType: 'x'
+      },
+
+      // Make sure connected countries have similar colors
+      colors: [
+        colors[0],
+        colors[1],
+        colors[2],
+        colors[3],
+        colors[4],
+        colors[5],
+        colors[6],
+        colors[7],
+        colors[8],
+        colors[9],
+        colors[0],
+        Highcharts.color(colors[4]).brighten(0.2).get(),
+        Highcharts.color(colors[7]).brighten(0.).get(),
+      ],
+
+      title: {
+        floating: false,
+        align: 'left',
+        text: `Area Burnt by Cause per Year in ${st0}`
+      },
+
+      credits: {
+        enabled: false
+      },
+
+      xAxis: {
+        maxPadding: 0,
+        type: 'category',
+        crosshair: true,
+        categories: data.streamGraph.years_categories.map(x => x),
+        labels: {
+          align: 'left',
+          reserveSpace: true,
+          rotation: 315,
+        },
+        lineWidth: 0,
+        margin: 20,
+        tickWidth: 0
+      },
+
+      yAxis: {
+        visible: false,
+        startOnTick: false,
+        endOnTick: false
+      },
+
+      legend: {
+        enabled: true,
+        align: 'center',
+        verticalAlign: 'top',
+
+      },
+      series: data.streamGraph.data
+    });
+  };
+
+
+  buildPieChart(D, st0);
+  buildBarChart(D, st0);
+  buildStreamGraph(D, st0);
+
+});
